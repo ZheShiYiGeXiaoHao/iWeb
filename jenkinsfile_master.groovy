83299aea7bd6a57ea12e1b1e3a7b5b1b785cdd48
ZheShiYiGeXiaoHao <46801937+ZheShiYiGeXiaoHao@users.noreply.github.com>
2019-04-17 14:55:30 +0800

    Update jenkinsfile_master.groovy

--- jenkinsfile_master.groovy
+++ jenkinsfile_master.groovy
@@ -11,6 +11,10 @@ stage('maven compile & package') {
         //定义maven java环境
         def mvnHome = tool 'maven-3.6.0_master'
         def jdkHome = tool 'jdk1.8_master'
+        env.PATH = "${mvnHome}/bin:${env.PATH}"
+        env.PATH = "${jdkHome}/bin:${env.PATH}"
+        sh "java -version"
+        sh "mvn -version"
         sh "mvn clean install"
         sh "mv target/iWeb.war target/ROOT.war"
     }
